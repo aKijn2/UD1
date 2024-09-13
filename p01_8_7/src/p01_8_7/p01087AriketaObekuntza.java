4964786895d7b9f581ae53b6b2f847294151fd8b
aKijn2 <mrkinder20041010@gmail.com>
2024-09-13 11:42:58 +0200

    3rd commit

--- p01_8_7/src/p01_8_7/p01087AriketaObekuntza.java
+++ p01_8_7/src/p01_8_7/p01087AriketaObekuntza.java
@@ -12,16 +12,16 @@ public class p01087AriketaObekuntza {
 
         int euroak = sc.nextInt();
 
-        // 200ko billeteak, 100ko billeteak, 10ko billeteak eta soberan zenbat diru dagoen kalkulatuko du
+        // 200ko billeteak, 20ko billeteak, 10ko billeteak eta soberan zenbat diru dagoen kalkulatuko du
         int berrehun = euroak / 200;
         int euroak2 = euroak % 200;
-        int ehun = euroak2 / 100;
-        int euroak3 = euroak2 % 100;
+        int ehun = euroak2 / 20;
+        int euroak3 = euroak2 % 20;
         int hamar = euroak3 / 10;
         int sobera = euroak3 % 10;
 
         System.out.println("200ko billeteak: " + berrehun);
-        System.out.println("100ko billeteak: " + ehun);
+        System.out.println("20ko billeteak: " + ehun);
         System.out.println("10ko billeteak: " + hamar);
         System.out.println("Soberan: " + sobera + " euro");
 
